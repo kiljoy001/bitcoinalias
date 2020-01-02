477a0511488cfb3eaa10934d564fbe4b98fe63a5
Scott Guyton <kiljoy001@gmail.com>
2020-01-01 17:10:07 -0800

    Added a few more tests, added some comments

--- bigchaindb/BitcoinAddressFormatChecker.py
+++ bigchaindb/BitcoinAddressFormatChecker.py
@@ -1,15 +1,20 @@
 import base58
 
 class Checker:
-    def __init__(self, address):
+    """Create checker, address optional if need to be reused"""
+    def __init__(self, address = None):
         self.bitcoin_address = address
 
     def check_address(self):
-        
+        """Checks address if it decodes correctly"""    
         try:
             base58.b58decode_check(self.bitcoin_address)
             return True
         
-        except ValueError as e:
-            print(e)
+        except ValueError:
+            #print(e)
             return False
+
+    def update_address(self, address):
+        """Updates property with a new address"""
+        self.bitcoin_address = address
--- bigchaindb/tests.py
+++ bigchaindb/tests.py
@@ -2,10 +2,26 @@ from django.test import TestCase, SimpleTestCase
 from .BitcoinAddressFormatChecker import Checker
 
 class Test_Bitcoin_Address_Format_Checker(SimpleTestCase):
+    Checker = Checker()
+
     def setUp(self):
         self.GoodAddress = '18V6RzeJ6wodhyQNAwrCdRD7mAUavggWkZ'
-        self.GoodCheck = Checker(self.GoodAddress)
+        self.BadChecksum = '17V6RzeJ6wodhyQNAwrCdRD7mAUavggWkZ'
+        self.BadTooFewChar ='6RzeJ6wodhyQN'
         return super().setUp()
+        
+    def tearDown(self):
+        Test_Bitcoin_Address_Format_Checker.Checker = Checker()
+        return super().tearDown()
 
     def test_good_address_returns_true(self):
-        self.assertTrue(self.GoodCheck.check_address())
+        Test_Bitcoin_Address_Format_Checker.Checker.update_address(self.GoodAddress)
+        self.assertTrue(Test_Bitcoin_Address_Format_Checker.Checker.check_address())
+    
+    def test_bad_checksum_returns_false(self):
+        Test_Bitcoin_Address_Format_Checker.Checker.update_address(self.BadChecksum)
+        self.assertFalse(Test_Bitcoin_Address_Format_Checker.Checker.check_address())
+
+    def test_too_few_characters_returns_false(self):
+        Test_Bitcoin_Address_Format_Checker.Checker.update_address(self.BadChecksum)
+        self.assertFalse(Test_Bitcoin_Address_Format_Checker.Checker.check_address())
